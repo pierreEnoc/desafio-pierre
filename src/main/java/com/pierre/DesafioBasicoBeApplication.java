55cf0693b8515733580ffc5a80dacc685b56e322
Pierre enoc <pierre.enoc@gympass.com>
2021-05-19 14:25:18 -0300

    :tada: project created

---
+++ src/main/java/com/pierre/DesafioBasicoBeApplication.java
@@ -0,0 +1,13 @@
+package com.pierre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioBasicoBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioBasicoBeApplication.class, args);
+	}
+
+}
